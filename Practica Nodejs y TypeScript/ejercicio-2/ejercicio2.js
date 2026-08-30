9fbc48054e5d94fd2f126f4bd92f70aa035a4ad7
tobisabbione <tobiiseba714@gmail.com>
2026-08-30 17:55:16 -0300

    Ejercicio 2 terminado

---
+++ Practica Nodejs y TypeScript/ejercicio-2/ejercicio2.js
@@ -0,0 +1,6 @@
+"use strict";
+function multiplicacion(n1, n2) {
+    return n1 * n2;
+}
+let resultado = multiplicacion(4, 6);
+console.log("El resultado de la multiplicacion es: ", resultado);
---
+++ Practica Nodejs y TypeScript/ejercicio-2/ejercicio2.ts
@@ -0,0 +1,7 @@
+function multiplicacion(n1: number, n2: number) : number{
+    return n1 * n2;
+}
+
+let resultado = multiplicacion(4, 6);
+
+console.log("El resultado de la multiplicacion es: ", resultado);
