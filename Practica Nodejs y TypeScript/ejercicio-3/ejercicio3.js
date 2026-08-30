30c3f3de2a4b9b9c78f26498488b9ed46b00e24f
tobisabbione <tobiiseba714@gmail.com>
2026-08-30 17:58:48 -0300

    Ejercicio 3 terminado

---
+++ Practica Nodejs y TypeScript/ejercicio-3/ejercicio3.js
@@ -0,0 +1,6 @@
+"use strict";
+/*  Definir una interfaz para una persona (nombre, edad).
+● Crear un objeto que use la interfaz y mostrar sus
+propiedades */
+let persona1 = { nombre: "Rodrigo", edad: 25 };
+console.log(persona1);
---
+++ Practica Nodejs y TypeScript/ejercicio-3/ejercicio3.ts
@@ -0,0 +1,8 @@
+interface Persona{
+    nombre: string;
+    edad: number;
+}
+
+let persona1: Persona = {nombre: "Rodrigo", edad: 25}
+
+console.log(persona1)
